9e09d960f0ab3facd8cac198f6518461831f09b5
Narek <narek1144@gmail.com>
2022-08-31 14:20:14 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// const jsonServer = require("json-server");
+
+// const server = jsonServer.create();
+// const router = jsonServer.router(db);
+// const middlewares = jsonServer.defaults();
+
+// server.use(router);
+// server.listen(PORT, () => {
+//   console.log("Server is running on port", PORT);
+// });
